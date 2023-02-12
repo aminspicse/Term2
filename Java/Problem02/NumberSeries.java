4f9eb0e1a7ce89ed0b02d9901f5d1c2d8e9b3527
Md. Al Amin <aminspicse@gmail.com>
2023-02-12 20:38:40 +0600

    Update Looping Statement

--- Java/Problem02/NumberSeries.java
+++ Java/Problem02/NumberSeries.java
@@ -5,9 +5,34 @@ import java.util.Scanner;
 public class NumberSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        for(int i = 2; i <= 20; i = i+2){
+        System.out.println("Enter N: ");
+        int n = sc.nextInt();
+
+        System.out.println("Using For Loop: ");
+        int sum=0;
+        for(int i = 2; i <= n; i = i+2){
             System.out.println(i);
+            sum = sum+i;
         }
+        System.out.println("Summation: "+sum);
+
+        System.out.println("Using While Loop: ");
+        int i=2;
+        while(i<=n){
+            System.out.println(i);
+            ///sum = sum+i;
+            i = i+2;
+        }
+        System.out.println("Summation: "+sum);
+
+        System.out.println("Using Do While: ");
+        int j=2;
+        do{
+            System.out.println(j);
+            //sum = sum+i;
+            j = j+2;
+        }while(j<=n);
+        System.out.println("Summation: "+sum);
+
     }
 }
